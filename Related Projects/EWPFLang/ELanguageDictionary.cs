69e8d81998f642b8bb44e7736be89c051028b4d3
Timor Gruber <timor.gruber@gmail.com>
2016-09-20 00:36:56 +0300

    Add new project named "EWPFLang", which provides multi-lingual support for the EWPF library. Project and its' classes are still not fully implemented.

---
+++ Related Projects/EWPFLang/ELanguageDictionary.cs
@@ -0,0 +1,47 @@
+﻿namespace EWPFLang
+{
+    /// <summary>
+    /// A static class used as a language dictionary, providing various strings used throughout the EWPF library in different languages.
+    /// </summary>
+    public static class ELanguageDictionary
+    {
+        #region Events
+
+        #endregion
+
+        #region Fields
+
+
+
+        #endregion
+
+        #region Constructors
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Loads a language file containing all language values stated as properties in this class based on the given language.
+        /// </summary>
+        /// <param name="i_Language">Language to load.</param>
+        public static void LoadLanguage(Language i_Language)
+        {
+
+        }
+
+        #endregion
+
+        #region Properties
+
+        public static string Yes { get; private set; }
+
+        public static string No { get; private set; }
+
+        public static string Cancel { get; private set; }
+
+        public static string OK { get; private set; }
+
+        #endregion
+    }
+}
---
+++ Related Projects/EWPFLang/Language.cs
@@ -0,0 +1,17 @@
+﻿namespace EWPFLang
+{
+    /// <summary>
+    /// An enum listing all languages supported by the EWPF library.
+    /// </summary>
+    public enum Language
+    {
+        EnglishUs,
+        EnglishUk,
+        French,
+        Italian,
+        Spanish,
+        Hebrew,
+        Deutch,
+        Russian
+    }
+}
