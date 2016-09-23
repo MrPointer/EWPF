04ff635abaf937e1f7c52a1a2899762eceae06eb
Timor Gruber <timor.gruber@gmail.com>
2016-09-23 14:42:40 +0300

    Modify Button's grid in EMessageBox, modify Button's template. Replace Button's grid to a StackPanel in EMessageBox since it prevents issues and overhead calculations. Modify general Button's template to use a border color pointing to DarkPrimaryColor instead of PrimaryColor since it has blended with the background.

--- Related Projects/EWPF_Test/MVVM/MainViewModel.cs
+++ Related Projects/EWPF_Test/MVVM/MainViewModel.cs
@@ -68,7 +68,7 @@ namespace EWPF_Test.MVVM
             if (MessageBoxService == null) return;
             MessageBoxService.Show(@"Test", @"Hello World! This is a sample content for a sample message box." +
             "\n" + "Did you like it? Please contribute the EWPF team online on github!",
-                MessageBoxButton.OKCancel, MessageBoxImage.Information);
+                MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
         }
 
         #endregion
