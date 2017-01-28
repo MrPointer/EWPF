4263a6670194f2fd5afd8edbbfee02af574a106b
Timor Gruber <timor.gruber@gmail.com>
2017-01-28 23:22:57 +0200

    Added a converter class to convert boolean values to their opposites. Also added some unit test for this class.

---
+++ Converters/InverseBoolConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EWPF.Converters
+{
+    /// <summary>
+    /// a converter class used to convert a boolean value to its' opposite value.
+    /// <para />
+    /// For example: True will become false.
+    /// </summary>
+    public class InverseBoolConverter : IValueConverter
+    {
+        #region Methods
+
+        /// <inheritdoc />
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value), @"Value to convert can't be null");
+            bool boolValue = (bool)value;
+            return !boolValue;
+        }
+
+        /// <inheritdoc />
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Convert(value, targetType, parameter, culture);
+        }
+
+        #endregion
+    }
+}
---
+++ Test Projects/EWPF_Tests/Unit/Converters/InverseBoolTests.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Globalization;
+using EWPF.Converters;
+using NUnit.Framework;
+
+namespace EWPF_Tests.Unit.Converters
+{
+    [TestFixture]
+    public class InverseBoolTests
+    {
+        #region Events
+
+
+
+        #endregion
+
+        #region Fields
+
+
+
+        #endregion
+
+        #region Constructors
+
+
+
+        #endregion
+
+        #region Methods
+
+        [Test]
+        public void Convert_NullValue_ThrowsArgumentNullException()
+        {
+            var inverseBoolConverter = new InverseBoolConverter();
+            Assert.Catch<ArgumentNullException>(
+                () => inverseBoolConverter.Convert(null, typeof(bool), 
+                null, CultureInfo.CurrentUICulture));
+        }
+
+        [Test]
+        public void Convert_TrueValue_ReturnsFalse()
+        {
+            var inverseBoolConverter = new InverseBoolConverter();
+            var convertedObject = inverseBoolConverter.Convert(true, typeof(bool),
+                null, CultureInfo.CurrentCulture);
+            bool convertedValue = convertedObject != null && (bool) convertedObject;
+            Assert.False(convertedValue);
+        }
+
+        #endregion
+
+        #region Properties
+
+
+
+#endregion
+    }
+}
