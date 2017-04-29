492092224678406912c5d28a243c218d350c6865
Timor Gruber <timor.gruber@gmail.com>
2017-04-29 19:34:59 +0300

    Modified the older 'ShowDialog' method of the 'IDialogService' interface. It now accepts an identifier as an object, aiding to find the dialog to display. It also accepts an optional 'DataContext' object. Added all the parameters that have been added before to the 'ShowDialog' method of the 'DialogUtility' class, to the 'ShowDialog' method of the 'IDialogService' interface.

--- MVVM/Services/IDialogService.cs
+++ MVVM/Services/IDialogService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Reflection;
 using System.Windows;
 using EWPF.Utility;
 using Microsoft.Win32;
@@ -22,8 +23,10 @@ namespace EWPF.MVVM.Services
         /// <summary>
         /// Shows an EWPF themed dialog on top of the visible window, filled with the given parameters.
         /// </summary>
-        /// <param name="i_Content">Dialog's content, which can be anything.</param>
-        /// <param name="i_Caption">Dialog's title/caption.</param>
+        /// <param name="i_Identifier">Reference to some sort of an identifier to aid in finding the correct dialog to display. <br />
+        ///     An enum for example is a good candidate.</param>
+        /// <param name="i_DataContext">Reference to an object that will be stored as 
+        /// the dialog's <see cref="FrameworkElement.DataContext"/>.</param>
         /// <param name="i_IsCenterOwner">Indicates weather the dialog will be displayed at the center of it's owner window or not.</param>
         /// <returns>Dialog result as a nullable boolean.</returns>
         /// <remarks><para>The method's design provides multiple approaches towards its' implementation.</para>
@@ -32,17 +35,21 @@ namespace EWPF.MVVM.Services
         /// <para>Another possible approach is to pass some kind of a template, probably represented by a code since this method
         /// is called from the view-model. The template will be the body of the dialog while the passed string will be its' title.</para>
         /// </remarks>
-        bool? ShowDialog(object i_Content, string i_Caption = null, bool i_IsCenterOwner = true);
+        bool? ShowDialog(object i_Identifier, object i_DataContext = null, bool i_IsCenterOwner = true);
 
         /// <summary>
         /// Displays a user-designed <see cref="Window"/> on top of the current one, reflecting to it as a modal dialog. <br />
         /// It's highly recommended to simply call the <see cref="DialogUtility.ShowDialog"/> method 
         /// that will find the correct dialog to display based on the <see cref="DialogAttribute"/> it has and the given name.
         /// </summary>
-        /// <param name="i_DialogName">Name of the dialog to open. 
-        /// Must be the same as stated in the <see cref="DialogAttribute"/>, if stated at all.</param>
-        /// <returns>Dialog result.</returns>
-        bool? ShowDialog(string i_DialogName);
+        /// <param name="i_DialogName">Name of the dialog to search, registered in its <see cref="DialogAttribute"/>.</param>
+        /// <param name="i_DataContext">Reference to an object that will be stored as 
+        ///     the dialog's <see cref="FrameworkElement.DataContext"/>.</param>
+        /// <param name="i_Namespace">Name of the specific namespace to search in. 
+        ///     Default is null, meaning the whole <see cref="Assembly"/> will be searched.</param>
+        /// <param name="i_AssemblyName">Name of the specific assembly to search in.</param>
+        /// <returns>Dialog result of the opened dialog.</returns>
+        bool? ShowDialog(string i_DialogName, object i_DataContext = null, string i_Namespace = null, string i_AssemblyName = null);
 
         /// <summary>
         /// Displays an <see cref="OpenFileDialog"/>, native to the Windows operating system, filtering the results to the given extensions.
