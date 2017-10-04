741d7e77e30ce77f503e0d26d08500bd3f6e2ae9
Timor Gruber <timor.gruber@gmail.com>
2017-10-04 18:46:55 +0300

    Added service to easily assign ViewModel instances to views.

---
+++ MVVM/Services/IViewModelService.cs
@@ -0,0 +1,28 @@
+﻿namespace EWPF.MVVM.Services
+{
+    /// <inheritdoc />
+    /// <summary>
+    /// An interface designed to assign ViewModel instances to the implementing view 
+    /// in a loose coupled way, providing an easy solution for ViewModel constructors that take parameters.
+    /// </summary>
+    public interface IViewModelService : IService
+    {
+        #region Fields
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Sets the given ViewModel to one of the view's properties, usually the DataContext.
+        /// </summary>
+        /// <param name="i_ViewModel">ViewModel instance to set.</param>
+        void SetViewModel(BaseViewModel i_ViewModel);
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+    }
+}
