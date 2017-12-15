b8e047920992563aeff8cbd6c42269e07155db81
Timor Gruber <timor.gruber@gmail.com>
2017-12-15 15:20:43 +0200

    Modified `IndefiniteProgressDialog` ViewModel to be generic. It's made generic to allow any type of return value from the executed progress. Implemented the progress execution using the execution-model of KIS.NET, allowing to execute progress in a separate task. Also implemented all required callbacks, including in the the case of cancellation.

--- MVVM/ViewModel/IndefiniteProgressDialogViewModel.cs
+++ MVVM/ViewModel/IndefiniteProgressDialogViewModel.cs
@@ -1,11 +1,21 @@
 ﻿using System;
+using System.Linq;
 using System.Threading;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
+using EWPF.Controls;
+using EWPF.MVVM.Services;
+using KISCore.Execution;
 
 namespace EWPF.MVVM.ViewModel
 {
-    public class IndefiniteProgressDialogViewModel : BaseViewModel
+    /// <summary>
+    /// A class representing a generic view model bound to the 
+    /// <see cref="IndefiniteProgressDialog"/> view.
+    /// </summary>
+    /// <typeparam name="TProgressResult">Type of the progressed action's result.</typeparam>
+    public class IndefiniteProgressDialogViewModel<TProgressResult> : BaseViewModel
     {
         #region Events
 
@@ -24,8 +34,15 @@ namespace EWPF.MVVM.ViewModel
         private const string cm_FLOW_DIRECTION_PROPERTY_NAME = "FlowDirection";
         private FlowDirection m_DialogFlowDirection;
 
-        private Action<CancellationToken> m_ProgressAction;
-        private Func<CancellationToken, bool> m_ProgressFunction;
+        private readonly ICancellableTaskExecutor<CancellationToken> m_TaskExecutor;
+
+        private readonly Action<CancellationToken> m_ProgressAction;
+        private readonly Action m_ActionCompletionCallback;
+
+        private readonly Func<CancellationToken, TProgressResult> m_ProgressFunction;
+        private readonly Action<TProgressResult> m_FunctionCompletionCallback;
+
+        private readonly Action m_CancellationCallback;
         private readonly CancellationToken m_ProgressCancellationToken;
 
         #region Commands
@@ -39,20 +56,30 @@ namespace EWPF.MVVM.ViewModel
         #region Constructors
 
         /// <inheritdoc />
-        public IndefiniteProgressDialogViewModel(Action<CancellationToken> i_ProgressAction,
-            CancellationToken i_CancellationToken, Action i_CancellationCallback,
-            Action i_CompletionCallback)
+        public IndefiniteProgressDialogViewModel(
+            ICancellableTaskExecutor<CancellationToken> i_TaskExecutor,
+            Action<CancellationToken> i_ProgressAction, CancellationToken i_CancellationToken,
+            Action i_CancellationCallback, Action i_ActionCompletionCallback)
         {
+            m_TaskExecutor = i_TaskExecutor;
             m_ProgressAction = i_ProgressAction;
             m_ProgressCancellationToken = i_CancellationToken;
+            m_CancellationCallback = i_CancellationCallback;
+            m_ActionCompletionCallback = i_ActionCompletionCallback;
         }
 
         /// <inheritdoc />
-        public IndefiniteProgressDialogViewModel(Func<CancellationToken, bool> i_ProgressFunction,
-            CancellationToken i_CancellationToken)
+        public IndefiniteProgressDialogViewModel(
+            ICancellableTaskExecutor<CancellationToken> i_TaskExecutor,
+            Func<CancellationToken, TProgressResult> i_ProgressFunction,
+            CancellationToken i_CancellationToken,
+            Action i_CancellationCallback, Action<TProgressResult> i_FunctionCompletionCallback)
         {
+            m_TaskExecutor = i_TaskExecutor;
             m_ProgressFunction = i_ProgressFunction;
             m_ProgressCancellationToken = i_CancellationToken;
+            m_CancellationCallback = i_CancellationCallback;
+            m_FunctionCompletionCallback = i_FunctionCompletionCallback;
         }
 
         #endregion
@@ -72,17 +99,48 @@ namespace EWPF.MVVM.ViewModel
         /// by executing the progress action set in the constructor.
         /// </summary>
         /// <param name="i_O">Irrelevant.</param>
-        private void HandleViewLoaded(object i_O)
+        private async void HandleViewLoaded(object i_O)
         {
-            if (m_ProgressAction != null)
-                m_ProgressAction(m_ProgressCancellationToken);
-            else if (m_ProgressFunction != null)
-                m_ProgressFunction(m_ProgressCancellationToken);
-            else
+            if (WindowService == null)
+                throw new NullReferenceException("Window service must be set");
+
+            if (m_ProgressAction == null && m_ProgressFunction == null)
                 throw new NullReferenceException("Progress action or function must be set");
+
+            try
+            {
+                if (m_ProgressAction != null)
+                {
+                    await m_TaskExecutor.Execute(m_ProgressAction, m_ProgressCancellationToken);
+                    m_ActionCompletionCallback();
+                }
+                else if (m_ProgressFunction != null)
+                {
+                    var progressResult = await m_TaskExecutor.Execute(m_ProgressFunction,
+                                             m_ProgressCancellationToken);
+                    m_FunctionCompletionCallback(progressResult);
+                }
+            }
+            catch (AggregateException aggregateException)
+            {
+                HandleFaultyExecution(aggregateException);
+            }
+            WindowService.CloseWindow(true);
         }
 
         #endregion
+
+        private void HandleFaultyExecution(AggregateException i_ThrownAggregateException)
+        {
+            if (i_ThrownAggregateException.InnerExceptions.Any(i_Exception =>
+                i_Exception.GetType() == typeof(TaskCanceledException)))
+            {
+                WindowService.CloseWindow(false);
+                m_CancellationCallback();
+            }
+            else
+                throw i_ThrownAggregateException;
+        }
 
         #endregion
 
@@ -126,6 +184,11 @@ namespace EWPF.MVVM.ViewModel
                 OnPropertyChanged(cm_FLOW_DIRECTION_PROPERTY_NAME, this);
             }
         }
+
+        /// <summary>
+        /// Gets or sets a service used to interact directly with the bound view's window.
+        /// </summary>
+        public IWindowService WindowService { get; set; }
 
         #region Commands
 
